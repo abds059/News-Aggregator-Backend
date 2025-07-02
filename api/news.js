0d90dcf6e746bdd5609992c90eaa27aa174a892e
Abdur Rehman Siddiqui <abdurrehmans059@gmail.com>
2025-07-02 10:44:41 +0500

    Updated news.js

--- api/news.js
+++ api/news.js
@@ -1,33 +1,4 @@
-// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-// export default async function handler(req, res) {
-//   const { q, topic } = req.query;
-
-//   const params = new URLSearchParams({
-//     token: process.env.GNEWS_API_KEY,
-//     lang: "en",
-//     country: "pk",
-//   });
-
-//   if (q) params.append("q", q);
-//   if (topic) params.append("topic", topic);
-
-//   const finalUrl = `https://gnews.io/api/v4/top-headlines?${params.toString()}`;
-
-//   try {
-//     const response = await fetch(finalUrl);
-//     const data = await response.json();
-
-//     res.setHeader("Access-Control-Allow-Origin", "https://abds059.github.io");
-//     res.status(200).json(data);
-//   } catch (error) {
-//     res.status(500).json({ error: "Failed to fetch news" });
-//   }
-// }
-
-
-// For Vercel, you don't need to dynamically import node-fetch
-// Just use the built-in fetch (Node 18+) or import it normally
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
